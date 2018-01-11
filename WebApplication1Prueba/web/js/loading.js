723feab0cfa756a79a5feb5c686e361f4146ad7e
LidianeLBCampos <lima.a.lidiane@gmail.com>
2018-01-11 09:30:40 +0100

    Botones Loading funcionan

--- WebApplication1Prueba/web/js/loading.js
+++ WebApplication1Prueba/web/js/loading.js
@@ -10,3 +10,18 @@ $(function () {
   }
  });
 });
+
+$(function () {
+ $('#register-submit').click(function () {
+  var username = $("#user_register").val();
+  var email = $("#email_register").val();
+  var password = $("#password_register").val();
+  var password_confirm = $("#confirm_password_register").val();
+  if (username !== "" && email !== "" && password !== "" && password_confirm !== "") {
+   setTimeout(function () {
+    document.getElementById('register-submit').style.visibility = 'hidden';
+    document.getElementById('cargando2').style.visibility = 'visible';
+   }, 200);
+  }
+ });
+});
